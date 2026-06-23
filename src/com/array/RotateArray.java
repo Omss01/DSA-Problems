3aa0550c2a0a6536afc6e014ab81d9bf2397c333
Om Shelke <shelkeom61@gmail.com>
2026-06-24 04:06:04 +0530

    Added Rotate Array Problem

---
+++ src/com/array/RotateArray.java
@@ -0,0 +1,47 @@
+package com.array;
+
+import java.util.Arrays;
+
+/*
+ * Time Complexity
+ * First Reverse -> reverse(nums, 0, n - 1); -> O(n)
+ * Second Reverse ->reverse(nums, 0, k - 1); -> O(k)
+ * Third Reverse -> reverse(nums, k, n - 1); ->	O(n-k)
+ * Total -> O(n) + O(k) + O(n-k) -> O(2n) (ignore constant) -> O(n)
+ * 
+ * Space Complexity -> O(1)
+ * */
+public class RotateArray {
+
+	public void rotate(int[] nums, int k) {
+
+		int n = nums.length;
+		k = k % n;
+
+		reverse(nums, 0, n - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, n - 1);
+	}
+
+	private void reverse(int[] nums, int start, int end) {
+
+		while (start < end) {
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
+
+			start++;
+			end--;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+
+		RotateArray obj = new RotateArray();
+		obj.rotate(nums, 3);
+
+		System.out.println(Arrays.toString(nums));
+	}
+}
